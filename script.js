6b3f149935f719a84cc617413d31e727a2f7aaa2
sbin <sbinsbin2@gmail.com>
2026-04-07 16:41:36 -0700

    cleaner part 1

--- script.js
+++ script.js
@@ -3,32 +3,23 @@
 // part 1
 let currentDate = new Date(Date.now());
 let dateText = "";
+let yearIn4Digits = currentDate.getFullYear() % 10000; // in case this is executed past 101st century for whatever reason
 
 if (currentDate.getMonth() < 9)
-{
     dateText += "0";
-}
 dateText += currentDate.getMonth() + 1;
 dateText += "/";
 if (currentDate.getDate() < 10)
-{
     dateText += "0";
-}
 dateText += currentDate.getDate();
 dateText += "/";
-if (currentDate.getFullYear() < 10)
-{
+if (yearIn4Digits < 10)
     dateText += "0";
-}
-if (currentDate.getFullYear() < 100)
-{
+if (yearIn4Digits < 100)
     dateText += "0";
-}
-if (currentDate.getFullYear() < 1000)
-{
+if (yearIn4Digits < 1000)
     dateText += "0";
-}
-dateText += currentDate.getFullYear(); // this will give a 5-digit year output by 101st century, which I'm not gonna bother fixing
+dateText += yearIn4Digits;
 document.getElementById("date").innerHTML = dateText;
 
 // part 2
