4a960cc989e551702639fc31840a68fed5977f54
sbin <sbinsbin2@gmail.com>
2026-04-07 17:28:42 -0700

    part 3

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // part 1
 let currentDate = new Date(Date.now());
-let yearIn4Digits = currentDate.getFullYear() % 10000; // in case this is executed past 101st century for whatever reason
+let yearInumber4Digits = currentDate.getFullYear() % 10000; // in case this is executed past 101st century for whatever reason
 let dateText = "";
 
 if (currentDate.getMonth() < 9)
@@ -13,28 +13,41 @@ if (currentDate.getDate() < 10)
     dateText += "0";
 dateText += currentDate.getDate();
 dateText += "/";
-if (yearIn4Digits < 10)
+if (yearInumber4Digits < 10)
     dateText += "0";
-if (yearIn4Digits < 100)
+if (yearInumber4Digits < 100)
     dateText += "0";
-if (yearIn4Digits < 1000)
+if (yearInumber4Digits < 1000)
     dateText += "0";
-dateText += yearIn4Digits;
+dateText += yearInumber4Digits;
 document.getElementById("date").innerHTML = dateText;
 
 // part 2
-let s0 = "123"
-let s1 = "456"
-let s2 = "asdf"
-let s3 = "7.89"
-let n0 = Number(s0);
-let n1 = Number(s1);
-let n2 = Number(s2);
-let n3 = Number(s3);
+let string0 = "123"
+let string1 = "456"
+let string2 = "asdf"
+let string3 = "7.89"
+let number0 = Number(string0);
+let number1 = Number(string1);
+let number2 = Number(string2);
+let number3 = Number(string3);
 let conversionText = "";
 
-conversionText += "\""+s0+"\": "+n0+" | isNaN: "+Number.isNaN(n0)+" | isInteger: "+Number.isInteger(n0)+"<br/>";
-conversionText += "\""+s1+"\": "+n1+" | isNaN: "+Number.isNaN(n1)+" | isInteger: "+Number.isInteger(n1)+"<br/>";
-conversionText += "\""+s2+"\": "+n2+" | isNaN: "+Number.isNaN(n2)+" | isInteger: "+Number.isInteger(n2)+"<br/>";
-conversionText += "\""+s3+"\": "+n3+" | isNaN: "+Number.isNaN(n3)+" | isInteger: "+Number.isInteger(n3)+"<br/>"; // isn't this literally what array is for?
+conversionText += "\""+string0+"\": "+number0+" | isNaN: "+Number.isNaN(number0)+" | isInteger: "+Number.isInteger(number0)+"<br/>";
+conversionText += "\""+string1+"\": "+number1+" | isNaN: "+Number.isNaN(number1)+" | isInteger: "+Number.isInteger(number1)+"<br/>";
+conversionText += "\""+string2+"\": "+number2+" | isNaN: "+Number.isNaN(number2)+" | isInteger: "+Number.isInteger(number2)+"<br/>";
+conversionText += "\""+string3+"\": "+number3+" | isNaN: "+Number.isNaN(number3)+" | isInteger: "+Number.isInteger(number3)+"<br/>"; // isn't this literally what arrays are for?
 document.getElementById("conversion").innerHTML = conversionText;
+
+// part 3
+let mathNum0 = Number.parseFloat("12345.67890abcdefg.hijklmnopqrs.tuvwxyz");
+let mathNum1 = Number.parseInt("0x7fff", 16);
+let mathNum2 = 12345;
+let formattingText = "";
+
+formattingText += "Number.parseFloat(\"12345.67890abcdefg.hijklmnopqrs.tuvwxyz\")<br/>= "+mathNum0+"<br/>";
+formattingText += "Number.parseInt(\"0x7fff\", 16)<br/>= "+mathNum1+"<br/>";
+formattingText += mathNum0+".toFixed(2)<br/>= "+mathNum0.toFixed(2)+"<br/>";
+formattingText += mathNum0+".toLocaleString(\"en-US\")<br/>= "+mathNum0.toLocaleString("en-US")+"<br/>";
+formattingText += mathNum0+".toLocaleString(\"de-DE\")<br/>= "+mathNum0.toLocaleString("de-DE")+"<br/>";
+document.getElementById("formatting").innerHTML = formattingText;
